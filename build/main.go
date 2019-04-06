e4e85deb29b288d54fa68f1d9b900d23f74e7be9
Jos Muysers <jos@muysers.nl>
2019-04-06 18:07:40 +0200

    build update

--- build/main.go
+++ build/main.go
@@ -12,8 +12,6 @@ import (
 	"github.com/tdewolff/minify/js"
 )
 
-var app components.App
-
 func main() {
 	var content string
 	var err error
@@ -23,7 +21,7 @@ func main() {
 	}
 	switch os.Args[1] {
 	case "less":
-		loadApp()
+		app := loadApp()
 		var i, j int
 		outPath := "static/css/components.less"
 		mainPath := "main.less"
@@ -35,7 +33,6 @@ func main() {
 		}
 		backCount := len(strings.Split(outPath, "/")) - 1
 		content = "// main: " + mainPath + "\n"
-
 		for _, cmp := range app.Components {
 			if len(cmp.LessFiles) > 0 {
 				for i = 0; i < len(cmp.LessFiles); i++ {
@@ -53,7 +50,7 @@ func main() {
 			fmt.Println("ERROR:", err)
 		}
 	case "js":
-		loadApp()
+		app := loadApp()
 		outPath := "static/js/"
 		var debug = false
 		var j int
@@ -144,10 +141,11 @@ func printHelp() {
 	fmt.Println("Build .js file from components: build js [outfile | debug] [debug]")
 	fmt.Println("Create new project from template: build new <name>")
 }
-func loadApp() {
+func loadApp() components.App {
 	var err error
-	conf := "app.json"
-	app := components.App{
+	var app components.App
+	conf := "app.json" //TODO: load from yaml file
+	app = components.App{
 		ConfigFile: conf,
 	}
 	err = app.LoadConfig()
@@ -157,8 +155,10 @@ func loadApp() {
 
 	}
 	err = app.LoadComponents()
+
 	if err != nil {
 		fmt.Println("ERROR LoadComponents:", err)
 		os.Exit(1)
 	}
+	return app
 }
