ac4f97d7a2f3c9628466725994bb444fb0eff2aa
Jos Muysers <jos@muysers.nl>
2019-08-17 13:30:58 +0200

    example login route

--- template/components/login/login.js
+++ template/components/login/login.js
@@ -1,20 +1,30 @@
-m.ui.components.login = (function(){
-    m.ui.ready(function(){
-        if (m.ui.jwt.authenticated()===true) {
+m.ui.components.login = (function () {
+    
+    m.ui.router.add(/\/login/, function (uri, data) {
+        var el = m.ui.components.import(document.createElement("login"));
+        el.template = "login";
+        el.render();
+        var main = document.querySelector("main");
+        main.innerHTML = "";
+        main.appendChild(el);
+    });
+
+    m.ui.ready(function () {
+        if (m.ui.jwt.authenticated() === true) {
             onLogin();
         }
     });
-    function onLogin(){
+    function onLogin() {
         console.log("LOGIN");
     }
-    return function(element, data){
-        element.login = function() {
-            m.ui.jwt.authenticate(element.getData("username"), element.getData("password")).then(function(){
+    return function (element, data) {
+        element.login = function () {
+            m.ui.jwt.authenticate(element.getData("username"), element.getData("password")).then(function () {
                 onLogin();
-            }).catch(function(err){
-                console.error("LOGIN:",err);
+            }).catch(function (err) {
+                console.error("LOGIN:", err);
             });
         }
-        return m.ui.component(element,data);
+        return m.ui.component(element, data);
     };
 }());
